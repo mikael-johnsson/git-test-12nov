812ae93847897cfa8b91b3d16e564efacf8817f4
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-11-12 13:27:39 +0100

    create createHtml

--- src/main.ts
+++ src/main.ts
@@ -16,4 +16,14 @@ fetch("http://www.omdbapi.com/?apikey=ffeb1c95&type=game&s=alan")
   .then((response) => response.json())
   .then((data: OmdbResponse) => {
     console.log(data);
+    createHtml(data.Search);
   });
+
+const createHtml = (games: Game[]) => {
+  games.forEach((game) => {
+    const gameContainer = document.createElement("div");
+    const imgContainer = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+  });
+};
